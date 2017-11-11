726fdf1a126594598cdc59a1754b4b475b934bae
Adam Malantonio <adam.malantonio@gmail.com>
2017-11-11 12:08:09 -0500

    add tweet building

---
+++ app-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  query: 'cat OR cats OR kitten OR kittens OR feline',
+
+  emoji: [
+    '🐱',
+    '😺',
+    '😸',
+    '😽',
+    '😻',
+    '😼',
+    '🐈',
+  ],
+
+  tweet_prefixes: [
+    'Meow!', 
+    'Purrrr!', 
+    'Prrrr!',
+    'Some kitten!',
+    'What a cat!',
+    'Fine Feline!',
+    'Such meow!',
+  ],
+
+  item_title_max_length: 100,
+}
--- server.js
+++ server.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const app = express()
+const { query } = require('./app-config')
+const randomItem = require('./lib/random-dpla-item')
+const createTweet = require('./lib/create-tweet')
+const blacklist = require('./lib/blacklist').openSync('./.data/blacklist.json')
+
+const api_key = process.env.DPLA_KEY
+const SECRET = process.env.SECRET_TOKEN
+
+app.get('/', (req, res) => {
+  res.json({status: '🐈'})
+})
 
 app.post('/tweet', (req, res) => {
+  const header = req.get('x-tweet-me-maybe')
 
+  if (!header || header !== SECRET) {
+    res.status(401)
+    return res.json({
+      error: 'unauthorized',
+      message: 'sorry friend, only authorized users plz',
+    })
+  }
+
+  randomItem(query, {blacklist, api_key})
+    .then(createTweet)
+    .then(tweet => {
+      res.json({tweet})
+    })
 })
+
+app.listen(8000)
